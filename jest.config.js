76bfcc55e94849c85f421e525fcb7a8f15161e08
Brian Le <brianle1301@gmail.com>
2020-10-15 13:54:45 +0100

    Update jest-config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@
 module.exports = {
     verbose: false,
     testRegex: 'tests/(?!utils\\.js).*\\.js',
+    testEnvironment: 'node',
     collectCoverageFrom: ['src/**/*.js'],
     coverageThreshold: {
         global: {
